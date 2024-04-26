7d25c4ba5b237a07765daa7e6790f2acc18fc0b8
jketterer02 <jketterer02@gmail.com>
2024-04-26 12:43:28 -0500

    works now

--- CLI.py
+++ CLI.py
@@ -9,10 +9,14 @@ if conn.is_connected(): print("Connection established...")
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
         
-# Execute the CREATE DATABASE SQL File
+
+# Read SQL commands from file
 with open("dbDDL.sql", 'r') as sql_file: sql_commands = sql_file.read()
 
-cursor.execute(sql_commands,multi=True)
+cursor.execute(sql_commands, multi=True)
+        
+# Commit the transaction
+conn.commit()
 
 # Close the cursor
 cursor.close()
